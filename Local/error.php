2b76202b3a42a4498802433c698ef4c43c5d7516
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-04 17:27:10 +0100

    test sur modification de fichier

--- Local/error.php
+++ Local/error.php
@@ -4,7 +4,7 @@
 <head> 
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title></title>
+  <title>Erreur</title>
   <link rel="stylesheet" href="">
 </head>
 <body>
