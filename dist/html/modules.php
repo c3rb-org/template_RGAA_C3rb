53fb8a3fd365fc2da91ca2b74ef0d7e3c5864989
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-06 16:56:38 +0100

    On genere ou non un aside autour du module et on lui applique le role adéquate

--- dist/html/modules.php
+++ dist/html/modules.php
@@ -15,21 +15,75 @@ function modChrome_CrbXhtml($module, &$params, &$attribs) {
 	?> 
 	<?php
 	//Variable utiles
-	$nbmod 				= $attribs['nbmod']; 					// Nombre de module dans la position
-	$modbssize 			= $params->get('bootstrap_size');		// Taille bootstrap defini dans l'administration
-	$modstyle			= $attribs['style'];					// Style du module choisi dans l'admin
-	$modposname			= $attribs['name'];						// Nom de la position du module choisi dans l'admin
-	$paramtmpl_debug	= $attribs['debug'];
-	$headerLvl 			= $params->get('header_tag');
+	$nbmod 				= $attribs['nbmod']; 					// Nombre de module dans la position.
+	$modbssize 			= $params->get('bootstrap_size');		// Taille bootstrap defini dans l'administration.
+	$modstyle			= $attribs['style'];					// Style du module choisi dans l'admin.
+	$modposname			= $attribs['name'];						// Nom de la position du module choisi dans l'admin.
+	$paramtmpl_debug	= $attribs['debug'];					// Parametre du debug
+	$moduletype			= $module->module;						// type de module.
+	$headerLvl 			= $params->get('header_tag');			// niveau de h dans le titre
 	$datacontent 		=
 	"
 					Nbre de module dans la position : <span class='label label-info'>$nbmod</span><br />
 					Taille bootstrap dans l\'admin : <span class='label label-info'>$modbssize</span><br />
 					Style du module : <span class='label label-info'>$modstyle</span><br />
+					Nom du module : <span class='label label-info'>$moduletype</span><br />
 					Nom de la positiondu module : <span class='label label-info'>$modposname</span>
-	"
-	
-	?>
+	";															//Contenu du debug
+
+	// On defini un role pour chaque type de module default joomla.
+	//mod_articles_archive				x
+	//mod_articles_categories			z							
+	//mod_articles_category				z							
+	//mod_articles_latest				z							
+	//mod_articles_news					z							
+	//mod_articles_popular				z			
+	//mod_banners						x	
+	//mod_breadcrumbs						-		
+	//mod_custom						x	
+	//mod_feed							x
+	//mod_finder							-	
+	//mod_footer							-	
+	//mod_languages						x
+	//mod_login							x
+	//mod_menu								-
+	//mod_random_image					x		
+	//mod_related_items					z		
+	//mod_search						x	
+	//mod_stats							z
+	//mod_syndicate						x	
+	//mod_tags_popular					z		
+	//mod_tags_similar					z		
+	//mod_users_latest					x		
+	//mod_weblinks						z	
+	//mod_whosonline					z		
+	//mod_wrapper						x	
+
+
+//	if ($moduletype == 'mod_articles_archive' || 'mod_users_latest' || 'mod_wrapper' || 'mod_syndicate' || 'mod_random_image' || 'mod_languages' || 'mod_feed' || 'mod_custom' || 'mod_banner') {
+//			$asidemod = 'note'; 			//parenthese au contenu
+//	} 
+//	elseif ($moduletype ==  'mod_search' || 'mod_menu') {
+//
+//	    $asidemod = 'search';
+//	}
+//	else {
+//	    $asidemod = 'complementary'; 	//complementaire au contenu
+//	}
+
+if (($moduletype == 'mod_articles_archive') || ($moduletype == 'mod_users_latest') || ($moduletype == 'mod_wrapper') || ($moduletype == 'mod_syndicate') || ($moduletype == 'mod_random_image') || ($moduletype == 'mod_languages') || ($moduletype == 'mod_feed') || ($moduletype == 'mod_custom') || ($moduletype == 'mod_banner') ) {
+	$activeasidemod = 1;
+	$asidemod = 'note';    
+} elseif ($moduletype == 'mod_search') {
+	$activeasidemod = 1;
+	$asidemod = 'search';  
+} elseif ($moduletype == 'mod_menu') {
+	$activeasidemod = 0; 
+} else {
+	$activeasidemod = 1;
+	$asidemod = 'complementary';  
+}
+?>
 
 	<?php if ($modbssize != 0) : ?>
 		<div class="col-xs-12 col-sm-<?php echo $modbssize; ?> col-md-<?php echo $modbssize; ?> col-lg-<?php echo $modbssize; ?>">
@@ -37,19 +91,24 @@ function modChrome_CrbXhtml($module, &$params, &$attribs) {
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<?php endif; ?>
 			<div id="Mod<?php echo $module->id; ?>" class="moduletable <?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?> ">
-				<aside role="">
+				<?php if ($activeasidemod == 1 ): ?>
+				<aside role="<?php echo $asidemod; ?>">
+				<?php endif ?>
+				
 					<!-- Titre du module -->
 					<?php if ($module->showtitle) : ?> 
 						<<?php echo $headerLvl; ?>><span id="label<?php echo $module->id; ?>"><?php echo JText::_( $module->title ); ?></span></<?php echo $headerLvl;  ?>>
 					<?php else : ?>
 						<<?php echo $headerLvl; ?> class="sr-only"><span id="label<?php echo $module->id; ?>"><?php echo JText::_( $module->title ); ?></span></<?php echo $headerLvl;  ?>>	
 					<?php endif; ?>
-					
+
 					<!-- Content du module -->
 					<div class="modcontent"> 
 						<?php echo $module->content; ?>
 					</div>
+				<?php if ($activeasidemod == 1 ): ?>
 				</aside>
+				<?php endif ?>
 			</div>
 			<?php if ($paramtmpl_debug == 1) : ?>	
 			<a tabindex="0" class="btn btn-sm btn-warning" role="button" data-html="true" data-toggle="popover" data-placement="bottom" data-trigger="focus" title="Paramètre du template" 
