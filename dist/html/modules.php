a3ca5f3865e7f370f9b1a9264b0427524ea30c7a
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-09 09:06:49 +0100

    correction encodage

--- dist/html/modules.php
+++ dist/html/modules.php
@@ -25,7 +25,7 @@ function modChrome_CrbXhtml($module, &$params, &$attribs) {
 	$datacontent 		=
 	"
 					Nbre de module dans la position : <span class='label label-info'>$nbmod</span><br />
-					Taille bootstrap dans l\'admin : <span class='label label-info'>$modbssize</span><br />
+					Taille bootstrap dans l'admin : <span class='label label-info'>$modbssize</span><br />
 					Style du module : <span class='label label-info'>$modstyle</span><br />
 					Nom du module : <span class='label label-info'>$moduletype</span><br />
 					Nom de la positiondu module : <span class='label label-info'>$modposname</span>
