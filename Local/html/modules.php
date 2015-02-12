dfc2f8de6a0228c664bf61e87eed286e83f689b4
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-12 17:10:40 +0100

    Ajout de la fonction native de joomla sur les balise html5, modification du système de génération de balise c3rbrgaa, nettoyage du fichier

--- Local/html/modules.php
+++ Local/html/modules.php
@@ -22,6 +22,8 @@ function modChrome_CrbXhtml($module, &$params, &$attribs) {
 	$paramtmpl_debug	= $attribs['debug'];					// Parametre du debug
 	$moduletype			= $module->module;						// type de module.
 	$headerLvl 			= $params->get('header_tag');			// niveau de h dans le titre
+	$moduleTag      	= $params->get('module_tag');			// Choix html5 de la balise du module
+
 	$datacontent 		=
 	"
 					Nbre de module dans la position : <span class='label label-info'>$nbmod</span><br />
@@ -31,58 +33,19 @@ function modChrome_CrbXhtml($module, &$params, &$attribs) {
 					Nom de la positiondu module : <span class='label label-info'>$modposname</span>
 	";															//Contenu du debug
 
-	// On defini un role pour chaque type de module default joomla.
-	//mod_articles_archive				x
-	//mod_articles_categories			z							
-	//mod_articles_category				z							
-	//mod_articles_latest				z							
-	//mod_articles_news					z							
-	//mod_articles_popular				z			
-	//mod_banners						x	
-	//mod_breadcrumbs						-		
-	//mod_custom						x	
-	//mod_feed							x
-	//mod_finder							-	
-	//mod_footer							-	
-	//mod_languages						x
-	//mod_login							x
-	//mod_menu								-
-	//mod_random_image					x		
-	//mod_related_items					z		
-	//mod_search						x	
-	//mod_stats							z
-	//mod_syndicate						x	
-	//mod_tags_popular					z		
-	//mod_tags_similar					z		
-	//mod_users_latest					x		
-	//mod_weblinks						z	
-	//mod_whosonline					z		
-	//mod_wrapper						x	
-
-
-//	if ($moduletype == 'mod_articles_archive' || 'mod_users_latest' || 'mod_wrapper' || 'mod_syndicate' || 'mod_random_image' || 'mod_languages' || 'mod_feed' || 'mod_custom' || 'mod_banner') {
-//			$asidemod = 'note'; 			//parenthese au contenu
-//	} 
-//	elseif ($moduletype ==  'mod_search' || 'mod_menu') {
-//
-//	    $asidemod = 'search';
-//	}
-//	else {
-//	    $asidemod = 'complementary'; 	//complementaire au contenu
-//	}
-
-if (($moduletype == 'mod_articles_archive') || ($moduletype == 'mod_users_latest') || ($moduletype == 'mod_wrapper') || ($moduletype == 'mod_syndicate') || ($moduletype == 'mod_random_image') || ($moduletype == 'mod_languages') || ($moduletype == 'mod_feed') || ($moduletype == 'mod_custom') || ($moduletype == 'mod_banner') ) {
-	$activeasidemod = 1;
-	$asidemod = 'note';    
-} elseif ($moduletype == 'mod_search') {
-	$activeasidemod = 1;
-	$asidemod = 'search';  
-} elseif ($moduletype == 'mod_menu') {
-	$activeasidemod = 0; 
-} else {
-	$activeasidemod = 1;
-	$asidemod = 'complementary';  
-}
+	//Suivant le type du module le aside est different
+	if (($moduletype == 'mod_users_latest') || ($moduletype == 'mod_banners') || ($moduletype == 'mod_wrapper') || ($moduletype == 'mod_syndicate') || ($moduletype == 'mod_random_image') || ($moduletype == 'mod_languages') || ($moduletype == 'mod_feed') || ($moduletype == 'mod_custom') || ($moduletype == 'mod_banner') ) {
+		$activeasidemod = 1;
+		$asidemod = 'note';    
+	} elseif ($moduletype == 'mod_search') {
+		$activeasidemod = 1;
+		$asidemod = 'search';  
+	} elseif ($moduletype == 'mod_menu') {
+		$activeasidemod = 0; 
+	} else {
+		$activeasidemod = 1;
+		$asidemod = 'complementary';  
+	}
 ?>
 
 	<?php if ($modbssize != 0) : ?>
@@ -91,12 +54,18 @@ if (($moduletype == 'mod_articles_archive') || ($moduletype == 'mod_users_latest
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<?php endif; ?>
 			<div id="Mod<?php echo $module->id; ?>" class="moduletable <?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?> ">
-				<?php if ($activeasidemod == 1 ): ?>
-				<aside role="<?php echo $asidemod; ?>">
-				<?php endif ?>
-				<?php if ($moduletype == 'mod_menu') {
-					echo '<nav role="navigation" aria-labelledby="label'.$module->id.'">';
-				} ?>
+
+				<?php if ($moduleTag == 'div') : ?>
+					<?php if ($activeasidemod == 1 ): ?>
+					<aside role="<?php echo $asidemod; ?>">
+					<?php endif ?>
+					<?php if ($moduletype == 'mod_menu') {
+						echo '<nav role="navigation" aria-labelledby="label'.$module->id.'">';
+					} ?>
+				<?php else : ?>
+					<<?php echo $moduleTag; ?>>
+				<?php endif; ?>
+
 					<!-- Titre du module -->
 					<?php if ($module->showtitle) : ?> 
 						<<?php echo $headerLvl; ?>><span id="label<?php echo $module->id; ?>"><?php echo JText::_( $module->title ); ?></span></<?php echo $headerLvl;  ?>>
@@ -108,12 +77,17 @@ if (($moduletype == 'mod_articles_archive') || ($moduletype == 'mod_users_latest
 					<div class="modcontent"> 
 						<?php echo $module->content; ?>
 					</div>
-				<?php if ($moduletype == 'mod_menu') {
-					echo '</nav>';
-				} ?>
-				<?php if ($activeasidemod == 1 ): ?>
-				</aside>
-				<?php endif ?>
+				<?php if ($moduleTag == 'div') : ?>
+					<?php if ($moduletype == 'mod_menu') {
+						echo '</nav>';
+					} ?>
+					<?php if ($activeasidemod == 1 ): ?>
+					</aside>
+					<?php endif ?>
+				<?php else : ?>
+					</<?php echo $moduleTag; ?>>
+				<?php endif; ?>
+
 			</div>
 			<?php if ($paramtmpl_debug == 1) : ?>	
 			<a tabindex="0" class="btn btn-sm btn-warning" role="button" data-html="true" data-toggle="popover" data-placement="bottom" data-trigger="focus" title="Paramètre du template" 
@@ -122,37 +96,6 @@ if (($moduletype == 'mod_articles_archive') || ($moduletype == 'mod_users_latest
 			</a>
 			<?php endif; ?>
 		</div>
-		<?php
-	}
-function modChrome_CrbXhtmlElastic($module, &$params, &$attribs) { 
-	?> 
-	<?php
-	//Variable utiles
-	$nbmod 		= $attribs['nbmod']; 					// Nombre de module dans la position
-	$modbssize 	= $params->get('bootstrap_size');		// Taille bootstrap defini dans l'administration
-	$modstyle	= $attribs['style'];					// Style du module choisi dans l'admin
-	$modposname	= $attribs['name'];						// Nom de la position du module choisi dans l'admin
-	$colbsauto	= 12/$nbmod;							// calcul auto de la taille bs 
-	?>
-
-			<div id="Mod<?php echo $module->id; ?>" class="moduletable <?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?> ">
-				<div class="col-xs-12 col-sm-<?php echo $colbsauto; ?> col-md-<?php echo $colbsauto; ?> col-lg-<?php echo $colbsauto; ?>">
-					<!-- Titre du module -->
-					<?php if ($module->showtitle) : ?> 
-						<<?php echo $headerLvl;  ?>><span id="label<?php echo $module->id; ?>"><?php echo JText::_( $module->title ); ?></span></<?php echo $headerLvl; ?>>
-					<?php endif; ?>
-					<!-- Content du module -->
-					<div class="modcontent">    
-						<?php echo $module->content; ?>
-					</div>
-					<div class="alert alert-info">
-						Nbre de module dans la position : <?php echo $nbmod; ?> <br /> 
-						Taille bootstrap dans l'admin : <?php echo $modbssize; ?> <br />
-						Style du module : <?php echo $modstyle; ?> <br />
-						Nom de la positiondu module : <?php echo $modposname; ?>
-					</div>
-				</div>		
-			</div>
 		<?php
 	}
 
