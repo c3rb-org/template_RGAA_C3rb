f360b3bffbf805a535f0a49f29a42f8ad73db6b6
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-12 17:13:55 +0100

    Agencement HTML5

---
+++ Local/html/mod_articles_category/c3rbrgaa.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_category
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<div class="category-module<?php echo $moduleclass_sfx; ?>"><!-- Pas de liste C3rbrgaa -->
+	<?php if ($grouped) : ?>
+		<?php foreach ($list as $group_name => $group) : ?>
+		<article role="article">
+			<div>
+				<?php foreach ($group as $item) : ?>
+					<div>
+						<?php if ($params->get('link_titles') == 1) : ?>
+							<header>
+								<h2>
+									<a class="mod-articles-category-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+									<?php echo $item->title; ?>
+									</a>
+								</h2>
+							</header>
+						<?php else : ?>
+							<header>
+								<h2>
+								<?php echo $item->title; ?>
+								</h2>
+							</header>
+						<?php endif; ?>
+						<?php if ( ($item->displayHits)||($params->get('show_author'))||($item->displayCategoryTitle)||($item->displayDate) ) : ?>
+						<address>
+						<div class="article-info-mod-cat"><!-- On armonise en css pour les affichages des details = aux affichages sur un article -->
+						<?php endif ?>	
+						<?php if ($item->displayHits) : ?>
+							<span class="mod-articles-category-hits">
+							<meta content="UserPageVisits:<?php echo $item->displayHits; ?>" itemprop="interactionCount">
+								<?php echo $item->displayHits; ?>
+							</span>
+						<?php endif; ?>
+	
+						<?php if ($params->get('show_author')) : ?>
+							<span class="mod-articles-category-writtenby" itemprop="name" >
+								<?php echo $item->displayAuthorName; ?>
+							</span>
+						<?php endif;?>
+						<?php if ($item->displayCategoryTitle) : ?>
+							<span class="mod-articles-category-category">
+							<a href="<?php echo $item->displayCategoryLink; ?>" itemprop="genre"><?php echo $item->category_title; ?></a><!-- modifie pour les microformat -->
+							</span>
+						<?php endif; ?>
+	
+						<?php if ($item->displayDate) : ?>
+							<span class="mod-articles-category-date">
+								<time datetime="<?php echo $item->displayDate; ?>" itemprop="dateCreated"><!-- Le format de datetime est pas au top pour les microformats -->
+									<?php echo $item->displayDate; ?>
+								</time>
+							</span>
+						<?php endif; ?>
+						<?php if ( ($item->displayHits)||($params->get('show_author'))||($item->displayCategoryTitle)||($item->displayDate) ) : ?>
+						</div>
+						</address>
+						<?php endif ?>
+						<?php if ($params->get('show_introtext')) : ?>
+							<p class="mod-articles-category-introtext">
+								<?php echo $item->displayIntrotext; ?>
+							</p>
+						<?php endif; ?>
+	
+						<?php if ($params->get('show_readmore')) : ?>
+							<footer>
+							<p class="mod-articles-category-readmore">
+								<a class="mod-articles-category-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+									<?php if ($item->params->get('access-view') == false) : ?>
+										<?php echo JText::_('MOD_ARTICLES_CATEGORY_REGISTER_TO_READ_MORE'); ?>
+									<?php elseif ($readmore = $item->alternative_readmore) : ?>
+										<?php echo $readmore; ?>
+										<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+											<?php if ($params->get('show_readmore_title', 0) != 0) : ?>
+												<?php echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit')); ?>
+											<?php endif; ?>
+									<?php elseif ($params->get('show_readmore_title', 0) == 0) : ?>
+										<?php echo JText::sprintf('MOD_ARTICLES_CATEGORY_READ_MORE_TITLE'); ?>
+									<?php else : ?>
+										<?php echo JText::_('MOD_ARTICLES_CATEGORY_READ_MORE'); ?>
+										<?php echo JHtml::_('string.truncate', ($item->title), $params->get('readmore_limit')); ?>
+									<?php endif; ?>
+								</a>
+							</p>
+							</footer>
+						<?php endif; ?>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</article>
+		<?php endforeach; ?>
+	
+	<?php else : ?>
+		<?php foreach ($list as $item) : ?>
+		<article role="article">
+			<div>
+				<header>
+				<?php if ($params->get('link_titles') == 1) : ?>
+					<h2>
+						<a class="mod-articles-category-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+						<?php echo $item->title; ?>
+						</a>
+					</h2>
+				<?php else : ?>
+					<h2><?php echo $item->title; ?></h2>
+				<?php endif; ?>
+				</header>
+
+				<?php if ( ($item->displayHits)||($params->get('show_author'))||($item->displayCategoryTitle)||($item->displayDate) ) : ?>
+				<address>
+				<div class="article-info-mod-cat"><!-- On armonise en css pour les affichages des details = aux affichages sur un article -->
+				<?php endif ?>
+				<?php if ($item->displayHits) : ?>
+					<span class="mod-articles-category-hits">
+					<meta content="UserPageVisits:<?php echo $item->displayHits; ?>" itemprop="interactionCount">
+						<?php echo $item->displayHits; ?>
+					</span>
+				<?php endif; ?>
+	
+				<?php if ($params->get('show_author')) : ?>
+					<span class="mod-articles-category-writtenby" itemprop="name" >
+						<?php echo $item->displayAuthorName; ?>
+					</span>
+				<?php endif;?>
+	
+				<?php if ($item->displayCategoryTitle) : ?>
+					<span class="mod-articles-category-category">
+						<a href="<?php echo $item->displayCategoryLink; ?>" itemprop="genre"><?php echo $item->category_title; ?></a><!-- modifie pour les microformat -->
+					</span>
+				<?php endif; ?>
+	
+				<?php if ($item->displayDate) : ?>
+					<span class="mod-articles-category-date">
+					<time datetime="<?php echo $item->displayDate; ?>" itemprop="dateCreated"><!-- Le format de datetime est pas au top pour les microformats -->
+						<?php echo $item->displayDate; ?>
+					</time>
+					</span>
+				<?php endif; ?>
+				<?php if ( ($item->displayHits)||($params->get('show_author'))||($item->displayCategoryTitle)||($item->displayDate) ) : ?>
+				</div>
+				</address>
+				<?php endif ?>
+				</header>
+				<?php if ($params->get('show_introtext')) : ?>
+					<p class="mod-articles-category-introtext">
+						<?php echo $item->displayIntrotext; ?>
+					</p>
+				<?php endif; ?>
+	
+				<?php if ($params->get('show_readmore')) : ?>
+				 <footer>
+					<p class="mod-articles-category-readmore">
+						<a class="mod-articles-category-title <?php echo $item->active; ?>" href="<?php echo $item->link; ?>">
+							<?php if ($item->params->get('access-view') == false) : ?>
+								<?php echo JText::_('MOD_ARTICLES_CATEGORY_REGISTER_TO_READ_MORE'); ?>
+							<?php elseif ($readmore = $item->alternative_readmore) : ?>
+								<?php echo $readmore; ?>
+								<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+							<?php elseif ($params->get('show_readmore_title', 0) == 0) : ?>
+								<?php echo JText::sprintf('MOD_ARTICLES_CATEGORY_READ_MORE_TITLE'); ?>
+							<?php else : ?>
+								<?php echo JText::_('MOD_ARTICLES_CATEGORY_READ_MORE'); ?>
+								<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+							<?php endif; ?>
+						</a>
+					</p>
+				</footer>
+				<?php endif; ?>
+			</div>
+		</article>
+		<?php endforeach; ?>
+	<?php endif; ?>
+</div>
---
+++ Local/html/mod_articles_popular/c3rbrgaa.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_popular
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<ul class="list-group mostread<?php echo $moduleclass_sfx; ?>">
+<?php foreach ($list as $item) : ?>
+	<li class="list-group-item"> 
+		<a href="<?php echo $item->link; ?>">
+			<?php echo $item->title; ?></a>
+	</li>
+<?php endforeach; ?>
+</ul>
