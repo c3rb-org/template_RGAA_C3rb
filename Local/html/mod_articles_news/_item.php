6d64e638af5eaabff5d688bb00119f8469eb61c2
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-02-25 12:06:49 +0100

    Merge maj Joomla! 3.4.0

---
+++ Local/html/mod_articles_news/_item.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_news
+ *	°version J! : 3.4.0 - MIR
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$item_heading = $params->get('item_heading', 'h4');
+?>
+<?php if ($params->get('item_title')) : ?>
+
+	<<?php echo $item_heading; ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
+	<?php if ($params->get('link_titles') && $item->link != '') : ?>
+		<a href="<?php echo $item->link; ?>">
+			<?php echo $item->title; ?>
+		</a>
+	<?php else : ?>
+		<?php echo $item->title; ?>
+	<?php endif; ?>
+	</<?php echo $item_heading; ?>>
+
+
+<?php endif; ?>
+
+<?php if (!$params->get('intro_only')) : ?>
+	<?php echo $item->afterDisplayTitle; ?>
+<?php endif; ?>
+
+<?php echo $item->beforeDisplayContent; ?>
+
+<?php echo $item->introtext; ?>
+
+<?php if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) : ?>
+	<?php echo '<a aria-label="' .JText::_('TPL_RGAAC3RB_ARTICLE_MORE '.$item->title.''). '" class="readmore" href="' . $item->link . '">' . $item->linkText . '</a>'; ?><!-- c3rbrgaa readmore accessible -->
+<?php endif; ?>
