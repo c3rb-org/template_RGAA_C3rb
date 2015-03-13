4a1d7c5c9ab05efac969d8ea7d925a0329edf365
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-03-13 09:19:05 +0100

    On écrit la Meta X-UA-Compatible hors du $doc car génère une erreur w3c

--- Local/index.php
+++ Local/index.php
@@ -2,8 +2,8 @@
 defined( '_JEXEC' ) or die; 
 
 // Parametre general joomla
-	$app            	= JFactory::getApplication();
-	$doc            	= JFactory::getDocument();							
+$app            	= JFactory::getApplication(); 
+$doc            	= JFactory::getDocument();							
 	$user            	= JFactory::getUser();								// Info Users
 	$this->language  	= $doc->language;									// Langue du template
 	$this->direction 	= $doc->direction;									// Direction du template (RTL, LTR)
@@ -15,7 +15,7 @@ defined( '_JEXEC' ) or die;
 	$bssize				= $params->get('bootstrap_size');					// Taille bootstrap dans l'admin
 	$headerlvl 			= $params->get('headerLevel');						// Niveau de titre du module choisi dans l'admin
 	$tmplpath 			= $this->baseurl.'/templates/'.$this->template;     // adresse du template
-	 
+	
 	$config 			= new JConfig(); 
 	$doc->setGenerator('');	 	 											// on supprime le generator content="Joomla! - Open Source Content Management" pour plus de securite
 // Parametre du template
@@ -41,8 +41,9 @@ defined( '_JEXEC' ) or die;
 	// Bootstrap
 	$doc->setMetaData( 'viewport', 'width=device-width, initial-scale=1' );
 	//$doc->setMetaData( 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' ); // Dans ce cas on n'autorise pas le zoom sur mobile
+	
 	// Force la derniere version de IE
-	$doc->setMetadata('x-ua-compatible','IE=edge,chrome=1');
+	//$doc->setMetadata('x-ua-compatible','IE=edge,chrome=1'); Commente car pas valide w3c, sauf manipulation IIS remplacer par ligne90
 
 // Le CSS
 	// Si on est en debug on vas chercher les elements non minifie.
@@ -87,188 +88,192 @@ defined( '_JEXEC' ) or die;
 		<meta name="msapplication-TileImage" content="/templates/images/template/favicon/mstile-144x144.png">
 		<meta name="msapplication-config" content="/templates/images/template/favicon/browserconfig.xml">
 		
+		<!--[if ie]>
+		<meta content='IE=8' http-equiv='X-UA-Compatible'/>
+		<![endif]-->
+
 		<!-- Fin favicon 
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
 	-->
-	</head>
-	<?php if ( $paramtmpl_html == 1) {
-		echo '<div class="debughtml">'; 
-	} ?>
-	<body> 
-	    <jdoc:include type="message" />
-		<!-- début du template -->
-		<a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
-		<div class="container">
-			<div class="row">
-				<header aria-label="<?php echo $titlesite; ?>" role="banner">
+</head>
+<?php if ( $paramtmpl_html == 1) {
+	echo '<div class="debughtml">'; 
+} ?>
+<body> 
+	<jdoc:include type="message" />
+	<!-- début du template -->
+	<a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+	<div class="container">
+		<div class="row">
+			<header aria-label="<?php echo $titlesite; ?>" role="banner">
 				<?php if ($paramtmpl_tmpltitlechoice == 1) : ?>
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<?php else : ?>
-				<div class="sr-only">
-				<?php endif ?>
-				<h1><?php echo $titlesite; ?></h1>
-				</div>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<?php else : ?>
+						<div class="sr-only">
+						<?php endif ?>
+						<h1><?php echo $titlesite; ?></h1>
+					</div>
 					<!-- Bloc top -->		
 					<?php
 					$nbmod =  $this->countModules('position-1');
 					if ($nbmod): ?>
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							<div class="row">
-								<jdoc:include type="modules" name="position-1" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-							</div>
-						</div>	
-					<?php endif; ?> 
-					<?php 				
-					$nbmod =  $this->countModules('position-2');
-					if ($nbmod): ?>
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							<div class="row">
-								<jdoc:include type="modules" name="position-2" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-							</div>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="row">
+							<jdoc:include type="modules" name="position-1" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
 						</div>
-					<?php endif; ?> 
-					<?php 
-					$nbmod =  $this->countModules('position-3');
-					if ($nbmod): ?>
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-							<div class="row">
-								<jdoc:include type="modules" name="position-3" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-							</div>
-						</div>
-					<?php endif; ?>
-				<div class="clearfix"></div>
-				</header>
-			</div>
-				<!-- Fin bloc top -->
-
-				<!-- Calcul taille colonne centrale (gauche / centre / droite) -->
-				    <?php 
-				    	$colbs = 12;
-    					if($this->countModules('position-4')>0)
-        				$colbs = $colbs - 3;
-    					if($this->countModules('position-5')>0)
-        				$colbs = $colbs - 3;
-        		 	?>
-				<!-- Fin calcul taille colonne centrale (gauche / centre / droite) -->
-		<div class="row">
-				
-				<!-- Col gauche -->
-				<?php 
-				$nbmod =  $this->countModules('position-4');
+					</div>	
+				<?php endif; ?>
+				<?php 				
+				$nbmod =  $this->countModules('position-2');
 				if ($nbmod): ?>
-				
-					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-						<div class="row">
-							<jdoc:include type="modules" name="position-4" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				
-				<?php endif; ?>
-		
-				<!-- Fin Col gauche -->
-
-				<!-- Col centre -->
-		<div class="col-xs-12 col-sm-<?php echo $colbs; ?> col-md-<?php echo $colbs; ?> col-lg-<?php echo $colbs; ?>" >
-			
-				<?php if ($this->countModules('position-6')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-6" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?> 
-
-				<?php if ($this->countModules('position-7')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-7" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?> 
-				<?php if ($this->countModules('position-8')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-8" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?> 
-
-				<div class="clearfix"></div>
-				<main role="main">
-				<!-- Le contenu principal injecte par joomla -->
-				<jdoc:include type="component" />
-				</main>
-				<!-- Fin Le contenu principal injecte par joomla -->
-				<div class="clearfix"></div>
-
-				<?php if ($this->countModules('position-9')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-9" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?> 
-				<?php if ($this->countModules('position-10')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-10" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?>
-				<?php if ($this->countModules('position-11')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-11" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?>
-			
-		</div> 
-				<!-- Fin Col centre -->
-
-				<!-- Col de droite -->
-			
-				<?php 
-				$nbmod =  $this->countModules('position-5');
-				if ($nbmod): ?>
-				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-					
-						<div class="row">
-							<jdoc:include type="modules" name="position-5" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="row">
+						<jdoc:include type="modules" name="position-2" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+					</div> 
 				</div>
-				<?php endif; ?> 
-				<!-- Fin Col de droite -->
-				<div class="clearfix"></div>
-		</div>
-
-
-				<?php if ($this->countModules('position-12')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-12" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?>
-				<?php if ($this->countModules('position-13')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-13" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?>
-				<?php if ($this->countModules('position-14')): ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<div class="row">
-							<jdoc:include type="modules" name="position-14" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
-						</div>
-					</div>
-				<?php endif; ?>
-				<!-- Bloc bas -->
-								
+			<?php endif; ?> 
+			<?php 
+			$nbmod =  $this->countModules('position-3');
+			if ($nbmod): ?> 
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
+				<div class="row">
+					<jdoc:include type="modules" name="position-3" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+				</div>
 			</div>
+		<?php endif; ?>
+		<div class="clearfix"></div> 
+	</header>
+</div>
+<!-- Fin bloc top -->
+
+<!-- Calcul taille colonne centrale (gauche / centre / droite) -->
+<?php 
+$colbs = 12;
+if($this->countModules('position-4')>0)
+	$colbs = $colbs - 3;
+if($this->countModules('position-5')>0)
+	$colbs = $colbs - 3;
+?>
+<!-- Fin calcul taille colonne centrale (gauche / centre / droite) -->
+<div class="row">
+	
+	<!-- Col gauche -->
+	<?php 
+	$nbmod =  $this->countModules('position-4');
+	if ($nbmod): ?>
+	
+	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 colgche">
+		<div class="row">
+			<jdoc:include type="modules" name="position-4" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+		</div>
+	</div>
+	
+<?php endif; ?>
+
+<!-- Fin Col gauche -->
+
+<!-- Col centre -->
+<div class="col-xs-12 col-sm-<?php echo $colbs; ?> col-md-<?php echo $colbs; ?> col-lg-<?php echo $colbs; ?> colctre" >
+	
+	<?php if ($this->countModules('position-6')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-6" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?> 
+
+	<?php if ($this->countModules('position-7')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-7" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?> 
+	<?php if ($this->countModules('position-8')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-8" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?> 
+
+	<div class="clearfix"></div>
+	<main role="main">
+		<!-- Le contenu principal injecte par joomla -->
+		<jdoc:include type="component" />
+	</main>
+	<!-- Fin Le contenu principal injecte par joomla -->
+	<div class="clearfix"></div>
+
+	<?php if ($this->countModules('position-9')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-9" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?> 
+	<?php if ($this->countModules('position-10')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-10" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?>
+	<?php if ($this->countModules('position-11')): ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="row">
+				<jdoc:include type="modules" name="position-11" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+			</div>
+		</div>
+	<?php endif; ?>
+	
+</div> 
+<!-- Fin Col centre -->
+
+<!-- Col de droite -->
+
+<?php 
+$nbmod =  $this->countModules('position-5');
+if ($nbmod): ?>
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 coldrte">
+	
+	<div class="row">
+		<jdoc:include type="modules" name="position-5" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+	</div>
+	
+</div>
+<?php endif; ?> 
+<!-- Fin Col de droite -->
+<div class="clearfix"></div>
+</div>
+
+
+<?php if ($this->countModules('position-12')): ?>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="row">
+			<jdoc:include type="modules" name="position-12" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+		</div>
+	</div>
+<?php endif; ?>
+<?php if ($this->countModules('position-13')): ?>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="row">
+			<jdoc:include type="modules" name="position-13" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+		</div>
+	</div>
+<?php endif; ?>
+<?php if ($this->countModules('position-14')): ?>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="row">
+			<jdoc:include type="modules" name="position-14" style="CrbXhtml" nbmod="<?php echo $nbmod; ?>" debug="<?php echo $paramtmpl_debug; ?>" />
+		</div>
+	</div>
+<?php endif; ?>
+<!-- Bloc bas -->		
+</div>
+
 <jdoc:include type="modules" name="debug" style="none" />
 
 <?php //require_once 'html/bootstrap.test.full.php'; ?>
@@ -278,27 +283,28 @@ defined( '_JEXEC' ) or die;
 <?php if ($paramtmpl_debug == 1) {
 	//require_once 'html/bootstrap.test.full.php'; 
 	//require_once 'html/font.test.full.php'; 	
-	} 
+} 
 ?>
 
 
+
+
  
-
-
 <?php 
 // Si on est en debug on vas chercher les elements non minifié.
 if($config->debug == 1) : ?>	
-	<!-- Si le debug on inclu les exemple botstrap -->
-	<script src="<?php echo $tmplpath; ?>/js/bootstrap.js" defer></script>
-	<script src="<?php echo $tmplpath; ?>/js/bootstrap-accessibility.js" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
-	<script src="<?php echo $tmplpath; ?>/js/stacktable.js" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
-	<script src="<?php echo $tmplpath; ?>/js/template.js" type="text/javascript" defer></script>
+<!-- Si le debug on inclu les exemple botstrap -->
+<script src="<?php echo $tmplpath; ?>/js/bootstrap.js" defer></script>
+<script src="<?php echo $tmplpath; ?>/js/bootstrap-accessibility.js" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
+<script src="<?php echo $tmplpath; ?>/js/stacktable.js" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
+<script src="<?php echo $tmplpath; ?>/js/template.js" type="text/javascript" defer></script>
 <?php else : ?> 
 	<script src="<?php echo $tmplpath; ?>/js/bootstrap.min.js" defer></script>
 	<!-- <script src="<?php echo $tmplpath; ?>/js/bootstrap-accessibility.min.js" defer></script> -->
 	<script src="<?php echo $tmplpath; ?>/js/stacktable.min.js" defer></script>
 	<script src="<?php echo $tmplpath; ?>/js/template.min.js" type="text/javascript" defer></script>
-<?php endif;?>
 
+<?php endif;?>
+ 
 </body>
-</html>
+</html> 
