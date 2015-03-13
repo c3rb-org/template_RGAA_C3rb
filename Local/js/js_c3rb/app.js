52819df2f8d5220b5b391a33fd248dcc7067d5cd
Vincent Espié <vincent.espie@gmail.com>
2015-03-13 10:06:55 +0100

    Ajout compatibilité Mootools => jQuery

--- Local/js/js_c3rb/app.js
+++ Local/js/js_c3rb/app.js
@@ -1,13 +1,15 @@
 jQuery(function ($) {
 	// Initialise les tooltip Bs
 	$('[data-toggle="tooltip"]').tooltip();
-	// Initialise le po-over
-	$('[data-toggle="popover"]').popover();
 	//Autorise le html dans les tools tip
 	$('[data-toggle="popover"]').popover({ html : true });
 	// dropdown
 	$('.dropdown-toggle').dropdown();
-
-	
 }); 
 
+
+Element.implement({
+	hide: function () { return this; },
+	show: function (v) { return this; },
+	slide: function (v) { return this; }
+});
