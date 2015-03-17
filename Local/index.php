d26d74fcda8a1c46685b3a9756eea8f634fa0952
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-03-17 17:22:52 +0100

    Modification arborescence dossier JS et modification appels dans index.php

--- Local/index.php
+++ Local/index.php
@@ -1,6 +1,6 @@
 <?php
 defined( '_JEXEC' ) or die; 
-
+ 
 // Parametre general joomla 
 $app            	= JFactory::getApplication(); 
 $doc            	= JFactory::getDocument();							
@@ -281,23 +281,27 @@ if ($nbmod): ?>
 	//require_once 'html/font.test.full.php'; 	
 } 
 ?>
-
-
-
-
  
 <?php 
 // Si on est en debug on vas chercher les elements non minifié.
 if(JDEBUG) : ?>	
 	<!-- Si le debug on inclu les exemple botstrap -->
-	<script src="<?php echo $tmplpath; ?>/js/bootstrap.js" type="text/javascript" defer></script>
-	<script src="<?php echo $tmplpath; ?>/js/bootstrap-accessibility.js" type="text/javascript" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
-	<script src="<?php echo $tmplpath; ?>/js/stacktable.js" type="text/javascript" defer></script> <!-- https://paypal.github.io/bootstrap-accessibility-plugin/demo.html -->
-	<script src="<?php echo $tmplpath; ?>/js/template.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/transition.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/alert.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/button.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/carousel.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/collapse.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/dropdown.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/modal.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/tooltip.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/popover.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/scrollspy.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/tab.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_bs/affix.js" type="text/javascript" defer></script>
+
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_uncompress/js_c3rb/app.js" type="text/javascript" defer></script>
+	<script src="<?php echo $tmplpath; ?>/js/js_uncompress/js_uncompress/bootstrap.js" type="text/javascript" defer></script>
 <?php else : ?> 
-	<script src="<?php echo $tmplpath; ?>/js/bootstrap.min.js" type="text/javascript" defer></script>
-	<!-- <script src="<?php echo $tmplpath; ?>/js/bootstrap-accessibility.min.js" type="text/javascript" defer></script> -->
-	<script src="<?php echo $tmplpath; ?>/js/stacktable.min.js" type="text/javascript" defer></script>
 	<script src="<?php echo $tmplpath; ?>/js/template.min.js" type="text/javascript" defer></script>
 <?php endif;?>
 
