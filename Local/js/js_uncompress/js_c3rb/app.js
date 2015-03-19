f84896c08163a82b0e84d2ad9ae50e388f32b7ad
Vincent Espié <vincent.espie@gmail.com>
2015-03-19 11:18:30 +0100

    Ajout javascript pour modale avec iframe

--- Local/js/js_uncompress/js_c3rb/app.js
+++ Local/js/js_uncompress/js_c3rb/app.js
@@ -1,14 +1,26 @@
 jQuery(function ($) {
+    
 	// Initialise les tooltip Bs
 	$('[data-toggle="tooltip"]').tooltip();
+	
 	//Autorise le html dans les tools tip
 	$('[data-toggle="popover"]').popover({ html : true });
+	
 	// dropdown
 	$('.dropdown-toggle').dropdown();
 
 	//noconflict chosen joomla
 	$('div.tab-pane-noconflictchosen').addClass( "tab-pane" ).removeClass( "tab-pane-noconflictchosen" );
 
+	//Modal avec iframe
+	$('div[data-iframe]').each(function(){
+	    $(this).on('show.bs.modal', function() {
+		var modalBody = $(this).find('.modal-body');
+		modalBody.find('iframe').remove();
+		modalBody.prepend('<iframe src="' + $(this).attr('data-iframe') + '" width="' + $(this).attr('data-iframe-width') + '" height="' + $(this).attr('data-iframe-height') + '"></iframe>');
+	    });
+	   
+	});
 }); 
 
 if(window.MooTools) 
