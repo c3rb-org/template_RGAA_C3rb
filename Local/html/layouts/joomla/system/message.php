3a31d57cb48cad4116288f517e00d5e4ff37f79a
Vincent Espié <vincent.espie@gmail.com>
2015-06-16 08:39:48 +0200

    Correction de l'affichage des messages, correspondance entre les types de messages joomla et les alertes de bootstrap 3

--- Local/html/layouts/joomla/system/message.php
+++ Local/html/layouts/joomla/system/message.php
@@ -1,42 +1,32 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  Layout
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
-
 $msgList = $displayData['msgList'];
-
+$error_type = array('error'=>'alert-danger','warning'=>'alert-warning',
+    'message'=>'alert-info');
 ?>
 <div class="container margetop">
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-<div id="system-message-container">
-	<?php if (is_array($msgList) && !empty($msgList)) : ?>
-		<div id="system-message">
-			<?php foreach ($msgList as $type => $msgs) : ?>
-							<div class="alert alert-info alert-<?php echo $type; ?>">
-					<?php // This requires JS so we should add it trough JS. Progressive enhancement and stuff. ?>
-					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  						<span aria-hidden="true">&times;</span>
-					</button>
-
-					<?php if (!empty($msgs)) : ?>
+			<div id="system-message-container">
+				<?php if (is_array($msgList) && !empty($msgList)) : ?>
+				<div id="system-message">
+				<?php foreach ($msgList as $type => $msgs) : ?>
+					<div role="alert" class="alert <?php echo (array_key_exists($type, $error_type) == true) ? $error_type[$type] : 'alert-info'; ?> alert-dismissible fade in">
+						<button aria-label="Fermer" data-dismiss="alert" class="close" type="button">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<?php if (!empty($msgs)) : ?>
 						<h4 class="alert-heading"><?php echo JText::_($type); ?></h4>
 						<div>
 							<?php foreach ($msgs as $msg) : ?>
-								<p><?php echo $msg; ?></p>
+							<p><?php echo $msg; ?></p>
 							<?php endforeach; ?>
 						</div>
-					<?php endif; ?>
+						<?php endif; ?>
+					</div>
+				<?php endforeach; ?>
 				</div>
-			<?php endforeach; ?>
-		</div>
-	<?php endif; ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
