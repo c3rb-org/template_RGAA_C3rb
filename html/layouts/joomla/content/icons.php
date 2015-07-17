af6a1d22b648ca8b1f65490a17f630d9cd77c56f
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-07-17 17:47:57 +0200

    Fix Accessibility rule for <a></a>

--- html/layouts/joomla/content/icons.php
+++ html/layouts/joomla/content/icons.php
@@ -2,23 +2,23 @@
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
- * °version J! : 3.4.3 - MIR
+ * Â°version J! : 3.4.3 - MIR
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('JPATH_BASE') or die;
-
 $canEdit = $displayData['params']->get('access-edit');
-
 ?>
-
+<div class="clearfix"></div>
 <div class="icons">
 	<?php if (empty($displayData['print'])) : ?>
-
 		<?php if ($canEdit || $displayData['params']->get('show_print_icon') || $displayData['params']->get('show_email_icon')) : ?>
-			<div class="btn-group pull-right"> 
-				<a title="<?php echo JText::_('TPL_RGAAC3RB_ARTICLE_OUTIL'); ?>" class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-cog"></span></a>
+			<div class="btn-group pull-right">
+				<a title="<?php echo JText::_('TPL_RGAAC3RB_ARTICLE_OUTIL_OPEN'); ?>" class="btn dropdown-toggle hasTooltip" data-toggle="dropdown" href="#">
+				<span class="glyphicon glyphicon-cog"></span>
+				<span class="sr-only"><?php echo JText::_('TPL_RGAAC3RB_ARTICLE_OUTIL'); ?></span>
+				</a>
+
 				<?php // Note the actions class is deprecated. Use dropdown-menu instead. ?>
 				<ul class="dropdown-menu">
 					<?php if ($displayData['params']->get('show_print_icon')) : ?>
@@ -33,12 +33,9 @@ $canEdit = $displayData['params']->get('access-edit');
 				</ul>
 			</div>
 		<?php endif; ?>
-
 	<?php else : ?>
-
 		<div class="pull-right">
 			<?php echo JHtml::_('icon.print_screen', $displayData['item'], $displayData['params']); ?>
 		</div>
-
 	<?php endif; ?>
 </div>
