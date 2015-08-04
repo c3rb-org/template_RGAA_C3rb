01ecbfb181b6210224cff903e23b6324e313a758
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-08-04 09:17:17 +0200

    Update feature layout with article image introduction

--- html/com_content/featured/default_item.php
+++ html/com_content/featured/default_item.php
@@ -23,37 +23,8 @@ $info    = $this->item->params->get('info_block_position', 0);
 <?php endif; ?>
 
 <header>
-<?php if ($params->get('show_title')) : ?>
-	<h3 class="item-title" itemprop="name">
-	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>" itemprop="url">
-			<?php echo $this->escape($this->item->title); ?>
-		</a>
-	<?php else : ?>
-		<?php echo $this->escape($this->item->title); ?>
-	<?php endif; ?>
-	</h3>
-<?php else: ?>
-	<h3 class="sr-only" itemprop="name">
-	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>" itemprop="url">
-			<?php echo $this->escape($this->item->title); ?>
-		</a>
-	<?php else : ?>
-		<?php echo $this->escape($this->item->title); ?>
-	<?php endif; ?>
-	</h3>
-<?php endif; ?>
-
-<?php if ($this->item->state == 0) : ?>
-	<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
-<?php endif; ?>
-<?php if (strtotime($this->item->publish_up) > strtotime(JFactory::getDate())) : ?>
-	<span class="label label-warning"><?php echo JText::_('JNOTPUBLISHEDYET'); ?></span>
-<?php endif; ?>
-<?php if ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate()) : ?>
-	<span class="label label-warning"><?php echo JText::_('JEXPIRED'); ?></span>
-<?php endif; ?>
+<?php //echo JLayoutHelper::render('joomla.content.intro_image', $this->item); ?> <!-- Passe dans joomla.content.blog_style_default_item_title pour agencement -->
+<?php echo JLayoutHelper::render('joomla.content.blog_style_default_item_title', $this->item); ?>
 
 <?php if ($canEdit || $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
 	<?php echo JLayoutHelper::render('joomla.content.icons', array('params' => $params, 'item' => $this->item, 'print' => false)); ?>
@@ -67,14 +38,7 @@ $info    = $this->item->params->get('info_block_position', 0);
 	<?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'above')); ?>
 		<?php endif; ?>
 </header>
-<?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
-	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
-	<?php if ($images->image_intro_caption):
-		echo 'class="caption"'.' title="' .htmlspecialchars($images->image_intro_caption) .'"';
-	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/> </div>
-<?php endif; ?>
+
 
 <?php if (!$params->get('show_intro')) : ?>
 	<?php echo $this->item->event->afterDisplayTitle; ?>
