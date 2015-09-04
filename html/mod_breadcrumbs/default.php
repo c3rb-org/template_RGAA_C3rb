6dd569f148680d7b7916bbbc143d2a25b725d7a7
Biromain <romain.bijakowski@gmail.com>
2015-09-04 09:52:19 +0200

    Fil d'Ariane uFormat
    Vérifier si Aria toujours valide.

--- html/mod_breadcrumbs/default.php
+++ html/mod_breadcrumbs/default.php
@@ -2,72 +2,40 @@
 /**
  * @package     Joomla.Site
  * @subpackage  mod_breadcrumbs
- * °version J! : 3.4.3 - MIR
+ * @version J! : 3.4.3 - MIR
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 JHtml::_('bootstrap.tooltip');
+
+$showHere = $params->get('showHere', 1);
+$showHome = $params->get('showHome', 1);
+$showLast = $params->get('showLast', 1);
+$separator = $params->get('separator', null);
+$homeText = $params->get('homeText', null);
 ?>
 
-<ul class="breadcrumb<?php echo $moduleclass_sfx; ?>" role="menu"> <!-- ARIA rgaac3rb -->
-	<?php
-	if ($params->get('showHere', 1))
-	{
-		echo '<li>' . JText::_('MOD_BREADCRUMBS_HERE') . '&#160;</li>';
-	}
-	else
-	{
-		echo '<li><span class="divider icon-location"></span></li>';
-	}
+<nav role="navigation">
+	<p id="breadcrumblabel"<?php echo $showHere?'':' class="hidden"'; ?>><?php echo JText::_('MOD_BREADCRUMBS_HERE'); ?></p>
+	<ol class="breadcrumb<?php echo $moduleclass_sfx; ?>" role="menu" aria-labelledby="breadcrumblabel" itemscope itemtype="http://schema.org/BreadcrumbList">
+		<?php $position=1; foreach($list as $key => $item): ?>
+			<?php $last = ($item === end($list)); $first = ($item === reset($list)); ?>
+			<?php $name = (!empty($homeText))?$homeText:$item->name; ?>
 
-	// Get rid of duplicated entries on trail including home page when using multilanguage
-	for ($i = 0; $i < $count; $i++)
-	{
-		if ($i == 1 && !empty($list[$i]->link) && !empty($list[$i - 1]->link) && $list[$i]->link == $list[$i - 1]->link)
-		{
-			unset($list[$i]);
-		}
-	}
+			<?php if(($first && $showHome) || ($last && $showLast) || (!$first || !$last)): ?>
+				<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" role="menuitem" <?php echo $last?' class="active"':''; ?>>
+					<meta itemprop="position" content="<?php echo $position; ?>" />
+					<?php if(!$last): ?><a itemprop="item" href="<?php echo $item->link; ?>" title="<?php echo $name; ?>"><?php endif; ?>
+						<span itemprop="name"><?php echo $name; ?></span>
+						<?php if(!$last): ?></a><?php endif; ?>
+				</li>
+			<?php endif; ?>
 
-	// Find last and penultimate items in breadcrumbs list
-	end($list);
-	$last_item_key = key($list);
-	prev($list);
-	$penult_item_key = key($list);
-
-	// Make a link if not the last item in the breadcrumbs
-	$show_last = $params->get('showLast', 1);
-
-	// Generate the trail
-	foreach ($list as $key => $item) :
-	if ($key != $last_item_key)
-	{
-		// Render all but last item - along with separator
-		echo '<li role="menuitem">'; // ARIA c3RBrgaa
-		if (!empty($item->link))
-		{
-			echo '<a href="' . $item->link . '" class="pathway">' . $item->name . '</a>';
-		}
-		else
-		{
-			echo '<span>' . $item->name . '</span>';
-		}
-
-		if (($key != $penult_item_key) || $show_last)
-		{
-			echo '<span class="divider">' . $separator . '</span>';
-		}
-
-		echo '</li>';
-	}
-	elseif ($show_last)
-	{
-		// Render last item if reqd.
-		echo '<li class="active">';
-		echo '<span>' . $item->name . '</span>';
-		echo '</li>';
-	}
-	endforeach; ?>
-</ul>
+			<?php if(!$last && !empty($separator)): ?>
+				<?php echo $separator; ?>
+			<?php endif; ?>
+			<?php $position++; endforeach; ?>
+	</ol>
+</nav>
