49682f875863edcbdec97df321865b43f8b9b418
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-09-15 16:00:32 +0200

    update message

--- html/layouts/joomla/system/message.php
+++ html/layouts/joomla/system/message.php
@@ -12,30 +12,30 @@ $msgList = $displayData['msgList'];
 $error_type = array('error'=>'alert-danger','warning'=>'alert-warning',
     'message'=>'alert-info');
 ?>
-<div class="container margetop">
-	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<div id="system-message-container">
-				<?php if (is_array($msgList) && !empty($msgList)) : ?>
-				<div id="system-message">
-				<?php foreach ($msgList as $type => $msgs) : ?>
-					<div role="alert" class="alert <?php echo (array_key_exists($type, $error_type) == true) ? $error_type[$type] : 'alert-info'; ?> alert-dismissible fade in">
-						<button aria-label="Fermer" data-dismiss="alert" class="close" type="button">
-							<span aria-hidden="true">&times;</span>
-						</button>
-						<?php if (!empty($msgs)) : ?>
-						<span class="alert-heading"><?php echo JText::_($type); ?></span><!-- Attention au nieveu de titre dans les alerte qui sont affiché en top du site  -->
-						<div>
-							<?php foreach ($msgs as $msg) : ?>
-							<p><?php echo $msg; ?></p>
-							<?php endforeach; ?>
+<?php if (is_array($msgList) && !empty($msgList)) : ?>
+	<div class="container margetop">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div id="system-message-container">
+					<div id="system-message">
+					<?php foreach ($msgList as $type => $msgs) : ?>
+						<div role="alert" class="alert <?php echo (array_key_exists($type, $error_type) == true) ? $error_type[$type] : 'alert-info'; ?> alert-dismissible fade in">
+							<button aria-label="Fermer" data-dismiss="alert" class="close" type="button">
+								<span aria-hidden="true">&times;</span>
+							</button>
+							<?php if (!empty($msgs)) : ?>
+							<span class="alert-heading"><?php echo JText::_($type); ?></span><!-- Attention au nieveu de titre dans les alerte qui sont affiché en top du site  -->
+							<div>
+								<?php foreach ($msgs as $msg) : ?>
+								<p><?php echo $msg; ?></p>
+								<?php endforeach; ?>
+							</div>
+							<?php endif; ?>
 						</div>
-						<?php endif; ?>
+					<?php endforeach; ?>
 					</div>
-				<?php endforeach; ?>
 				</div>
-				<?php endif; ?>
 			</div>
 		</div>
 	</div>
-</div>
+<?php endif; ?>
