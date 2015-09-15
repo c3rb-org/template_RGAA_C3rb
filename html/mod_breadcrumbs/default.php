34e0a65c2dc88d282416129eb0c842becd45e743
Biromain <romain.bijakowski@gmail.com>
2015-09-15 14:04:31 +0200

    Some fixes

--- html/mod_breadcrumbs/default.php
+++ html/mod_breadcrumbs/default.php
@@ -8,34 +8,54 @@
  */
 
 defined('_JEXEC') or die;
-JHtml::_('bootstrap.tooltip');
+//JHtml::_('bootstrap.tooltip');
 
-$showHere = $params->get('showHere', 1);
-$showHome = $params->get('showHome', 1);
-$showLast = $params->get('showLast', 1);
+$showHere = (bool) $params->get('showHere', true);
+$showHome = (bool) $params->get('showHome', true);
+$showLast = (bool) $params->get('showLast', true);
 $separator = $params->get('separator', null);
 $homeText = $params->get('homeText', null);
+
+// Copy array for manipulations
+$listMod = $list;
+
+// Set property home = true if home
+if($showHome) {
+	$list[0]->home = true;
+}// Else : Joomla already remove item if home not showed
+
+// Set property last = true if last
+if(!$showLast) {
+	//$listMod = array_pop($listMod); // Doesn't work as expected
+	unset($listMod[count($listMod)-1]);
+}else {
+	$listMod[count($listMod)-1]->last = true;
+}
+
+// Add position to all items
+if(!empty($listMod)) {
+	array_walk($listMod, function(&$arr, $key, $startNum=1){
+		$arr->position = $startNum++;
+	});
+}
+
 ?>
 
+<?php if( !empty($listMod) ): ?>
 <nav role="navigation">
 	<p id="breadcrumblabel"<?php echo $showHere?'':' class="hidden"'; ?>><?php echo JText::_('MOD_BREADCRUMBS_HERE'); ?></p>
 	<ol class="breadcrumb<?php echo $moduleclass_sfx; ?>" role="menu" aria-labelledby="breadcrumblabel" itemscope itemtype="http://schema.org/BreadcrumbList">
-		<?php $position=1; foreach($list as $key => $item): ?>
-			<?php $last = ($item === end($list)); $first = ($item === reset($list)); ?>
-			<?php $name = (!empty($homeText))?$homeText:$item->name; ?>
-
-			<?php if(($first && $showHome) || ($last && $showLast) || (!$first || !$last)): ?>
-				<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" role="menuitem" <?php echo $last?' class="active"':''; ?>>
-					<meta itemprop="position" content="<?php echo $position; ?>" />
-					<?php if(!$last): ?><a itemprop="item" href="<?php echo $item->link; ?>" title="<?php echo $name; ?>"><?php endif; ?>
-						<span itemprop="name"><?php echo $name; ?></span>
-						<?php if(!$last): ?></a><?php endif; ?>
-				</li>
-			<?php endif; ?>
-
-			<?php if(!$last && !empty($separator)): ?>
+		<?php foreach($listMod as $key => $item): ?>
+		<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" role="menuitem" <?php echo !empty($item->last)?' class="active"':''; ?>>
+			<meta itemprop="position" content="<?php echo $item->position; ?>" />
+			<?php if(empty($item->last)): ?><a itemprop="item" href="<?php echo $item->link; ?>" title="<?php echo $item->name; ?>"><?php endif; ?>
+				<span itemprop="name"><?php echo $item->name; ?></span>
+			<?php if(empty($item->last)): ?></a><?php endif; ?>
+		</li>
+			<?php if(empty($item->last) && !empty($separator)): ?>
 				<?php echo $separator; ?>
 			<?php endif; ?>
-			<?php $position++; endforeach; ?>
+		<?php endforeach; ?>
 	</ol>
 </nav>
+<?php endif; ?>
