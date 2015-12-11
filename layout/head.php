492c5a2da82c6eb7e3f702b04ec6f55e1f2a3b96
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2015-12-11 11:58:39 +0100

    Update Favicon call (like realfavicon preco.) in head of template

--- layout/head.php
+++ layout/head.php
@@ -1,29 +1,28 @@
 <?php defined( '_JEXEC' ) or die; ?>
 <jdoc:include type="head" /><!-- Balise meta et autre de la config du site joomla -->
-<!-- A FAIRE : supprimmer le caption.js genere par le jdoc:include ? -->
-<!-- Les favicons autre que la favicon.ico generale genere par le jdoc:include -->
-<link rel="apple-touch-icon" sizes="57x57" href="/templates/images/template/favicon/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/templates/images/template/favicon/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/templates/images/template/favicon/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/templates/images/template/favicon/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/templates/images/template/favicon/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/templates/images/template/favicon/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/templates/images/template/favicon/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/templates/images/template/favicon/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/templates/images/template/favicon/apple-touch-icon-180x180.png">
-<!-- <link rel="shortcut icon" href="/templates/images/template/favicon/favicon.ico"> genere par joomla  le $doc -->
-<link rel="icon" type="image/png" href="/templates/images/template/favicon/favicon-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/templates/images/template/favicon/favicon-160x160.png" sizes="160x160">
-<link rel="icon" type="image/png" href="/templates/images/template/favicon/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/templates/images/template/favicon/favicon-16x16.png" sizes="16x16">
-<link rel="icon" type="image/png" href="/templates/images/template/favicon/favicon-32x32.png" sizes="32x32">
-<meta name="msapplication-TileColor" content="#2b5797">
-<meta name="msapplication-TileImage" content="/templates/images/template/favicon/mstile-144x144.png">
-<meta name="msapplication-config" content="/templates/images/template/favicon/browserconfig.xml">
+
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tmplpath ?>/images/template/favicon/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="<?php echo $tmplpath ?>/images/template/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo $tmplpath ?>/images/template/favicon/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="<?php echo $tmplpath ?>/images/template/favicon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo $tmplpath ?>/images/template/favicon/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="<?php echo $tmplpath ?>/images/template/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo $tmplpath ?>/images/template/favicon/manifest.json">
+<link rel="mask-icon" href="<?php echo $tmplpath ?>/images/template/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="<?php echo $tmplpath ?>/images/template/favicon/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="<?php echo $tmplpath ?>/images/template/favicon/mstile-144x144.png">
+<meta name="msapplication-config" content="<?php echo $tmplpath ?>/images/template/favicon/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+
 <!--[if ie]>
 <meta content='IE=8' http-equiv='X-UA-Compatible'/>
 <![endif]-->
-<!-- Fin favicon 
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
--->
