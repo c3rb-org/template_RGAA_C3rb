8f0129882f94700d1086888a8219c6e5a08c3093
cyrilcab <cyrilcab12@gmail.com>
2016-01-04 10:54:49 +0100

    Icones email et impression accessible dans les articles

---
+++ html/layouts/joomla/content/helpers/rgaaicon.php
@@ -0,0 +1,66 @@
+<?php
+abstract class JHtmlRgaaicon
+{
+	public static function email($article, $params, $attribs = array(), $legacy = false)
+	{
+		require_once JPATH_SITE . '/components/com_mailto/helpers/mailto.php';
+		$uri      = JUri::getInstance();
+		$base     = $uri->toString(array('scheme', 'host', 'port'));
+		$template = JFactory::getApplication()->getTemplate();
+		$link     = $base . JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false);
+		$url      = 'index.php?option=com_mailto&tmpl=component&template=' . $template . '&link=' . MailToHelper::addLink($link);
+		$status = 'width=400,height=350,menubar=yes,resizable=yes';
+		if ($params->get('show_icons'))
+		{
+			if ($legacy)
+			{
+				$text = JHtml::_('image', 'system/emailButton.png', JText::_('JGLOBAL_EMAIL'), null, true);
+			}
+			else
+			{
+				$text = '<span class="icon-envelope"></span>' . JText::_('JGLOBAL_EMAIL');
+			}
+		}
+		else
+		{
+			$text = JText::_('JGLOBAL_EMAIL');
+		}
+		$attribs['title']   = JText::_('TPL_C3RB_RGAA_ARTICLE_EMAIL_TITLE');
+		$attribs['onclick'] = "window.open(this.href,'win2','" . $status . "'); return false;";
+		$attribs['rel']     = 'nofollow';
+		$output = JHtml::_('link', JRoute::_($url), $text, $attribs);
+		return $output;
+	}
+	
+	public static function print_popup($article, $params, $attribs = array(), $legacy = false)
+	{
+		$app = JFactory::getApplication();
+		$input = $app->input;
+		$request = $input->request;
+		$url  = ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language);
+		$url .= '&tmpl=component&print=1&layout=default&page=' . @ $request->limitstart;
+		$status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
+		// Checks template image directory for image, if non found default are loaded
+		if ($params->get('show_icons'))
+		{
+			if ($legacy)
+			{
+				$text = JHtml::_('image', 'system/printButton.png', JText::_('JGLOBAL_PRINT'), null, true);
+			}
+			else
+			{
+				$text = '<span class="icon-print"></span>' . JText::_('JGLOBAL_PRINT');
+			}
+		}
+		else
+		{
+			$text = JText::_('JGLOBAL_PRINT');
+		}
+		$attribs['title']   = JText::_('TPL_C3RB_RGAA_ARTICLE_PRINT_TITLE');
+		$attribs['onclick'] = "window.open(this.href,'win2','" . $status . "'); return false;";
+		$attribs['rel']     = 'nofollow';
+		return JHtml::_('link', JRoute::_($url), $text, $attribs);
+	}
+	
+	
+}
--- html/layouts/joomla/content/icons.php
+++ html/layouts/joomla/content/icons.php
@@ -7,70 +7,10 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('JPATH_BASE') or die;
+
+JHtml::addIncludePath(JPATH_THEMES . '/' . JFactory::getApplication()->getTemplate() . '/html/layouts/joomla/content/helpers');
+
 $canEdit = $displayData['params']->get('access-edit');
-
-//On redefini la function pour une gestion correcte des titles
-//https://github.com/joomla/joomla-cms/issues/8694  --- Check for update
-function rgaa_email($article, $params, $attribs = array(), $legacy = false)
-{
-	require_once JPATH_SITE . '/components/com_mailto/helpers/mailto.php';
-	$uri      = JUri::getInstance();
-	$base     = $uri->toString(array('scheme', 'host', 'port'));
-	$template = JFactory::getApplication()->getTemplate();
-	$link     = $base . JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false);
-	$url      = 'index.php?option=com_mailto&tmpl=component&template=' . $template . '&link=' . MailToHelper::addLink($link);
-	$status = 'width=400,height=350,menubar=yes,resizable=yes';
-	if ($params->get('show_icons'))
-	{
-		if ($legacy)
-		{
-			$text = JHtml::_('image', 'system/emailButton.png', JText::_('JGLOBAL_EMAIL'), null, true);
-		}
-		else
-		{
-			$text = '<span class="icon-envelope"></span>' . JText::_('JGLOBAL_EMAIL');
-		}
-	}
-	else
-	{
-		$text = JText::_('JGLOBAL_EMAIL');
-	}
-	$attribs['title']   = JText::_('TPL_C3RB_RGAA_ARTICLE_EMAIL_TITLE');
-	$attribs['onclick'] = "window.open(this.href,'win2','" . $status . "'); return false;";
-	$attribs['rel']     = 'nofollow';
-	$output = JHtml::_('link', JRoute::_($url), $text, $attribs);
-	return $output;
-}
-
-function rgaa_print_popup($article, $params, $attribs = array(), $legacy = false)
-	{
-		$app = JFactory::getApplication();
-		$input = $app->input;
-		$request = $input->request;
-		$url  = ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language);
-		$url .= '&tmpl=component&print=1&layout=default&page=' . @ $request->limitstart;
-		$status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
-		// Checks template image directory for image, if non found default are loaded
-		if ($params->get('show_icons'))
-		{
-			if ($legacy)
-			{
-				$text = JHtml::_('image', 'system/printButton.png', JText::_('JGLOBAL_PRINT'), null, true);
-			}
-			else
-			{
-				$text = '<span class="icon-print"></span>' . JText::_('JGLOBAL_PRINT');
-			}
-		}
-		else
-		{
-			$text = JText::_('JGLOBAL_PRINT');
-		}
-		$attribs['title']   = JText::_('TPL_C3RB_RGAA_ARTICLE_PRINT_TITLE');
-		$attribs['onclick'] = "window.open(this.href,'win2','" . $status . "'); return false;";
-		$attribs['rel']     = 'nofollow';
-		return JHtml::_('link', JRoute::_($url), $text, $attribs);
-	}
 
 ?>
 
@@ -89,12 +29,12 @@ function rgaa_print_popup($article, $params, $attribs = array(), $legacy = false
 					<?php if ($displayData['params']->get('show_print_icon')) : ?>
 						<li class="print-icon"> 
 						<?php //joomla code -- echo JHtml::_('icon.print_popup', $displayData['item'], $displayData['params']); ?>
-						<?php echo rgaa_print_popup($displayData['item'], $displayData['params']); ?>
+						<?php echo JHtml::_('rgaaicon.print_popup', $displayData['item'], $displayData['params']); ?>
 						</li>
 					<?php endif; ?>
 					<?php if ($displayData['params']->get('show_email_icon')) : ?>
 						<li class="email-icon">
-							<?php echo rgaa_email($displayData['item'], $displayData['params']); ?>
+							<?php echo JHtml::_('rgaaicon.email', $displayData['item'], $displayData['params']); ?>
 						</li>
 					<?php endif; ?>
 					<?php if ($canEdit) : ?>
