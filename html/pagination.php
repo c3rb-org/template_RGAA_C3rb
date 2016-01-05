e2682906b8b8695f7d7befbe336c361a2440ca90
Vincent Espié <vincent.espie@gmail.com>
2016-01-05 12:02:24 +0100

    Fixes and improvements for pagination

--- html/pagination.php
+++ html/pagination.php
@@ -1,52 +1,22 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  Templates.protostar
- *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die;
-/**
- * Renders the pagination footer
- *
- * @param   array   $list  Array containing pagination footer
- *
- * @return  string         HTML markup for the full pagination footer
- *
- * @since   3.0
- */
+
 function pagination_list_footer($list)
 {
-
-	$html = "<nav role=\"navigation\"><div class=\"pagination\">\n";
-	$html .= $list['pageslinks'];
-	$html .= "\n<input type=\"hidden\" name=\"" . $list['prefix'] . "limitstart\" value=\"" . $list['limitstart'] . "\" />";
-	$html .= "\n</div></nav>";
-
-	return $html;
+	$html = array();
+	$html[] = '<nav role="navigation">';
+	$html[] = '<div class="pagination">';
+	$html[] = $list['pageslinks'];
+	$html[] = '<input type="hidden" name="'.$list['prefix'].'limitstart" value="'.$list['limitstart'].'" />';
+	$html[] = '</nav>';
+	$html[] = '</div>';
+	return implode('',$html);
 }
-
-/**
- * Renders the pagination list
- *
- * @param   array   $list  Array containing pagination information
- *
- * @return  string         HTML markup for the full pagination object
- *
- * @since   3.0
- */
 
 function pagination_list_render($list)
 {
-
-	// Calculate to display range of pages
 	$currentPage = 1;
-	$range = 1;
-	$step = 5;
-	if(JFactory::getApplication()->input->get('layout') == 'vignette')
-		$step = 12;
-
+		
 	foreach ($list['pages'] as $k => $page)
 	{
 		if (!$page['active'])
@@ -54,49 +24,28 @@ function pagination_list_render($list)
 			$currentPage = $k;
 		}
 	}
-	if ($currentPage >= $step)
-	{
-		if ($currentPage % $step == 0)
-		{
-			$range = ceil($currentPage / $step) + 1;
-		}
-		else
-		{
-			$range = ceil($currentPage / $step);
-		}
-	}
-
-
+	
 	$html = array();
 	$html[] = '<div class="pagination">';
 	$html[] = '<div class="pagination-page text-left">';
 	$html[] = '<div class="btn-group">';
+	$html[] = '<nav role="navigation" aria-label="Pagination">';
 	$html[] = $list['start']['data'];
 	$html[] = $list['previous']['data'];
 
-	$html[]= '
-	<div class="dropdown btn btn-default pagi">
-	<nav role="navigation" aria-label="Pagination">
-	<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Page '.$currentPage .'<span class="caret"></span></button>
-	<ul class="dropdown-menu">';
+	$html[] = '<div class="dropdown btn btn-default pagi">';
+	$html[] = '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Page '.$currentPage .'<span class="caret"></span></button>';
+	$html[] = '<ul class="dropdown-menu">';
 
 	foreach ($list['pages'] as $k => $page)
 	{
-		if (in_array($k, range($range * $step - ($step + 1), $range * $step)))
-		{
-			if (($k % $step == 0 || $k == $range * $step - ($step + 1)) && $k != $currentPage && $k != $range * $step - $step)
-			{
-				$page['data'] = preg_replace('#(<a.*?>).*?(</a>)#', '$1...$2', $page['data']);
-			}
-		}
-
 		$html[] = $page['data'];
 	}
 
-	$html[] = '</ul></nav></div>';
+	$html[] = '</ul></div>';
 	$html[] = $list['next']['data'];
 	$html[] = $list['end']['data'];
-	$html[] = '</div>';
+	$html[] = '</div></nav>';
 	$html[] = '</div>';
 	$html[] = '</div>';
 	return implode('',$html);
@@ -136,47 +85,32 @@ function pagination_item_active(&$item)
 	return '<a href="' . $item->link . '" title="'.$title.'" class="btn btn-default hidden-phone hasTooltip">' . $display . '</a>';
 }
 
-/**
- * Renders an inactive item in the pagination block
- *
- * @param   JPaginationObject  $item  The current pagination object
- *
- * @return  string  HTML markup for inactive item
- *
- * @since   3.0
- */
 function pagination_item_inactive(&$item)
 {
-	// Check for "Start" item
 	if ($item->text == JText::_('JLIB_HTML_START'))
 	{
 		return '<a class="btn btn-default disabled" href="#" aria-label="First"><span aria-hidden="true" class="glyphicon glyphicon-fast-backward"></span><span class="sr-only">'. JText::_('TPL_C3RB_RGAA_PREM_PAGE') .'</span></a>';
 	}
 
-	// Check for "Prev" item
 	if ($item->text == JText::_('JPREV'))
 	{
 		return '<a class="btn btn-default disabled" href="#" aria-label="Previous"><span aria-hidden="true" class="glyphicon glyphicon-step-backward"></span><span class="sr-only">'. JText::_('TPL_C3RB_RGAA_PREC_PAGE') .'</span></a>';
 	}
 
-	// Check for "Next" item
 	if ($item->text == JText::_('JNEXT'))
 	{
 		return '<a class="btn btn-default disabled" href="#" aria-label="Next"><span aria-hidden="true" class="glyphicon glyphicon-step-forward"></span><span class="sr-only">'. JText::_('TPL_C3RB_RGAA_PAGE_SUIV') .'</span></a>';
 	}
 
-	// Check for "End" item
 	if ($item->text == JText::_('JLIB_HTML_END'))
 	{
 		return '<a class="btn btn-default disabled" href="#" aria-label="End"><span aria-hidden="true" class="glyphicon glyphicon-fast-forward"></span><span class="sr-only">'. JText::_('TPL_C3RB_RGAA_DER_PAGE') .'</span></a>';
 	}
 
-	// Check if the item is the active page
 	if (isset($item->active) && ($item->active))
 	{
 		return '<li class="active hidden-phone"><a href="#">' . $item->text . '</a></li>';
 	}
 
-	// Doesn't match any other condition, render a normal item
 	return '<li class="disabled hidden-phone"><a href="#">' . $item->text . '</a></li>';
 }
