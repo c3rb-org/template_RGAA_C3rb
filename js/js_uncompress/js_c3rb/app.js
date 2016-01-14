4ffe4844304c5815880e2e473128d383511c90ff
cyrilcab <cyrilcab12@gmail.com>
2016-01-14 17:50:27 +0100

    Modification gestion tooltipCompression de D:/Joomla3/templates/c3rb_rgaa/js/js_uncompress/js_c3rb/app.js Messages de compression : [INFO] Using charset UTF-8
    [WARNING] in D:/Joomla3/templates/c3rb_rgaa/js/js_uncompress/js_c3rb/app.js
      Try to use a single 'var' statement per scope.
    (this).attr("data-iframe"); ---> var  <--- modalBody=$(this).find(".modal-body"
    
    [WARNING] in D:/Joomla3/templates/c3rb_rgaa/js/js_uncompress/js_c3rb/app.js
      Try to use a single 'var' statement per scope.
    (this).find(".modal-body"); ---> var  <--- button=$(event.relatedTarget);if
    
    Compression terminée

--- js/js_uncompress/js_c3rb/app.js
+++ js/js_uncompress/js_c3rb/app.js
@@ -1,11 +1,13 @@
 jQuery(function ($) {
 	
     // Initialise les tooltip Bs
-    $('[data-toggle="tooltip"]').tooltip();
+    //$('[data-toggle="tooltip"]').tooltip();
+	$(document).tooltip({selector:'[data-toggle="tooltip"]'});
     
     //Autorise le html dans les tools tip
-    $('[data-toggle="popover"]').popover({html: true});
-
+    //$('[data-toggle="popover"]').popover({html: true});
+    $(document).popover({selector: '[data-toggle="popover"]', html: true});
+    
     // dropdown
     $('.dropdown-toggle').dropdown();
 
@@ -32,20 +34,36 @@ function load_modal_iframe(selector)
 {
 	(function ($) {
 		$(selector).each(function () {
-	$(this).on('show.bs.modal', function (event) {
-	    var url = $(this).attr('data-iframe');
-	    var modalBody = $(this).find('.modal-body');
-	    var button = $(event.relatedTarget);
-	    if (button.data('iframe'))
-		url = button.data('iframe');
-	    modalBody.find('iframe').remove();
-	    modalBody.prepend('<iframe style="border:none" src="' + url + '" width="' + $(this).attr('data-iframe-width') + '" height="' + $(this).attr('data-iframe-height') + '"></iframe>');
-	}).on('hide.bs.modal', function () {
-	    var modalBody = $(this).find('.modal-body');
-	    modalBody.find('iframe').remove();
-	});
-    });
+			$(document).on('show.bs.modal', selector, function (event) {
+			    var url = $(this).attr('data-iframe');
+			    var modalBody = $(this).find('.modal-body');
+			    var button = $(event.relatedTarget);
+			    if (button.data('iframe'))
+				url = button.data('iframe');
+			    modalBody.find('iframe').remove();
+			    modalBody.prepend('<iframe style="border:none" src="' + url + '" width="' + $(this).attr('data-iframe-width') + '" height="' + $(this).attr('data-iframe-height') + '"></iframe>');
+			}).on('hide.bs.modal', function () {
+			    var modalBody = $(this).find('.modal-body');
+			    modalBody.find('iframe').remove();
+			});
+	    });
 	})(jQuery);
+	/*(function ($) {
+		$(selector).each(function () {
+			$(this).on('show.bs.modal', function (event) {
+			    var url = $(this).attr('data-iframe');
+			    var modalBody = $(this).find('.modal-body');
+			    var button = $(event.relatedTarget);
+			    if (button.data('iframe'))
+				url = button.data('iframe');
+			    modalBody.find('iframe').remove();
+			    modalBody.prepend('<iframe style="border:none" src="' + url + '" width="' + $(this).attr('data-iframe-width') + '" height="' + $(this).attr('data-iframe-height') + '"></iframe>');
+			}).on('hide.bs.modal', function () {
+			    var modalBody = $(this).find('.modal-body');
+			    modalBody.find('iframe').remove();
+			});
+	    });
+	})(jQuery);*/
 }
 
 if (window.MooTools)
