8213aa61c44a4e36efd1bf60d7a18b8787d7eb8f
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2016-03-15 16:48:05 +0100

    improve accessibility on empty title

--- js/js_uncompress/js_c3rb/app.js
+++ js/js_uncompress/js_c3rb/app.js
@@ -3,6 +3,10 @@ jQuery(function ($) {
     // Initialise les tooltip Bs
     //$('[data-toggle="tooltip"]').tooltip();
 	$(document).tooltip({selector:'[data-toggle="tooltip"]'});
+
+  //Suppression des title vide générés par les tooltips
+    $('[data-original-title][title=""]').removeAttr('title');
+
     
     //Autorise le html dans les tools tip
     //$('[data-toggle="popover"]').popover({html: true});
