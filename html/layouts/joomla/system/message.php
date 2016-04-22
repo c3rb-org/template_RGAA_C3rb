e6c0419f8bf22b55efa5c58329278bae1c5d8555
Ruzafa Mikael MIR <mikael.r@cr2a-graphique.com>
2016-04-22 14:24:59 +0200

    Increase alert box accessibility

--- html/layouts/joomla/system/message.php
+++ html/layouts/joomla/system/message.php
@@ -2,7 +2,7 @@
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
- * °version J! : 3.4.3 - MIR
+ * Â°version J! : 3.4.3 - MIR
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -13,7 +13,7 @@ $error_type = array('error'=>'alert-danger','warning'=>'alert-warning',
     'message'=>'alert-info');
 ?>
 <?php if (is_array($msgList) && !empty($msgList)) : ?>
-<div class="messagejoomla">	
+<div class="messagejoomla" role="alert">	
 	<div class="container margetop">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -22,17 +22,18 @@ $error_type = array('error'=>'alert-danger','warning'=>'alert-warning',
 					<?php foreach ($msgList as $type => $msgs) : ?>
 						<div class="alert <?php echo (array_key_exists($type, $error_type) == true) ? $error_type[$type] : 'alert-info'; ?> alert-dismissible fade in">
 							<button data-dismiss="alert" class="close" type="button">
-								<span aria-hidden="true">&times;</span>
+								<span class="fa fa-times" aria-hidden="true" ></span>
 								<span class="sr-only">Fermer la boite d'alerte</span>
 							</button>
 							<?php if (!empty($msgs)) : ?>
-							<span id="dialogTitre" class="alert-heading"><?php echo JText::_($type); ?></span><!-- Attention au nieveu de titre dans les alerte qui sont affiché en top du site  -->
-							<div>
+							<span id="dialogTitre" class="alert-heading"><?php echo JText::_($type); ?></span>
+							<div id="dialogCt">
 								<?php foreach ($msgs as $msg) : ?>
-								<p id="addrole" aria-labelledby="dialogTitre"><?php echo $msg; ?></p>
+								<p ><?php echo $msg; ?></p>
 								<?php endforeach; ?>
 							</div>
 							<?php endif; ?>
+							<div aria-labelledby="dialogTitre dialogCt"></div>
 						</div>
 					<?php endforeach; ?>
 					</div>
