9dfe09bc9476c0ae59b6362fcd5a92528ff17b8f
v.espie <v.espie@c3rb.fr>
2019-02-18 11:20:37 +0100

    Corrections formulaire et icons

--- html/layouts/joomla/content/helpers/rgaaicon.php
+++ html/layouts/joomla/content/helpers/rgaaicon.php
@@ -3,11 +3,21 @@ abstract class JHtmlRgaaicon
 {
 	public static function email($article, $params, $attribs = array(), $legacy = false)
 	{
-		require_once JPATH_SITE . '/components/com_mailto/helpers/mailto.php';
+                require_once JPATH_SITE . '/components/com_mailto/helpers/mailto.php';
 		$uri      = JUri::getInstance();
 		$base     = $uri->toString(array('scheme', 'host', 'port'));
 		$template = JFactory::getApplication()->getTemplate();
-		$link     = $base . JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false);
+                
+                if($article->repetitions)
+                {
+                        JLoader::register('C3rbevenementHelperRoute', JPATH_ROOT . '/components/com_c3rbevenement/helpers/route.php');
+                        $link = $base . JRoute::_(C3rbevenementHelperRoute::getEventRoute($article->id.':'.$article->alias, $article->catid, $article->language), false);
+                }
+                else 
+                {
+                        $link = $base . JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false);
+                }
+                
 		$url      = 'index.php?option=com_mailto&tmpl=component&template=' . $template . '&link=' . MailToHelper::addLink($link);
 		$status = 'width=400,height=350,menubar=yes,resizable=yes';
 		if ($params->get('show_icons'))
@@ -34,11 +44,22 @@ abstract class JHtmlRgaaicon
 	
 	public static function print_popup($article, $params, $attribs = array(), $legacy = false)
 	{
-		$app = JFactory::getApplication();
+                $app = JFactory::getApplication();
 		$input = $app->input;
 		$request = $input->request;
-		$url  = ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language);
-		$url .= '&tmpl=component&print=1&layout=default&page=' . @ $request->limitstart;
+                
+                if($article->repetitions)
+                {
+                        JLoader::register('C3rbevenementHelperRoute', JPATH_ROOT . '/components/com_c3rbevenement/helpers/route.php');
+                        $url = C3rbevenementHelperRoute::getEventRoute($article->id.':'.$article->alias, $article->catid, $article->language);
+                        $url .= '&tmpl=component&print=1';
+                }
+                else
+                {
+                        $url  = ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language);
+                        $url .= '&tmpl=component&print=1&layout=default&page=' . @ $request->limitstart;
+                }
+                
 		$status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
 		// Checks template image directory for image, if non found default are loaded
 		if ($params->get('show_icons'))
