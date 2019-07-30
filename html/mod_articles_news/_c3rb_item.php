8257eb4001e07e78feff74ba15d926e09fbb0ef9
v.espie <v.espie@c3rb.fr>
2019-07-30 18:42:43 +0200

    Ajout de la mise en page "c3rb" pour le module "Flash d'informations" (paramètres d'ordre, colonnes, etc.)

---
+++ html/mod_articles_news/_c3rb_item.php
@@ -0,0 +1,85 @@
+﻿<?php
+defined('_JEXEC') or die;
+
+$item_heading = $params->get('item_heading', 'h4');
+?>
+<div class="<?php echo $colDef[$nbCols]; ?>">
+
+<div class="item-articlenews">
+    <?php foreach($orderInfos as $infos): ?>
+    <?php
+        switch($infos)
+        {
+            case 'image':
+                if(!empty($item->imageSrc)):
+                ?>
+                <div class="item-articlenews-image">
+                    <a href="<?php echo $item->link; ?>">
+                        <img src="<?php echo $item->imageSrc; ?>" alt="" />
+                        <span class="sr-only"><?php echo $item->imageCaption; ?></span>
+                    </a>
+                </div>
+                <?php
+                endif;
+                break;
+
+            case 'categorie':
+                ?>
+                <div class="item-articlenews-categorie"><?php echo $item->category_title; ?></div>
+                <?php
+                break;
+
+            case 'title':
+                ?>
+                <div class="item-articlenews-title">
+                    <<?php echo $item_heading; ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
+	                <?php if ($item->link !== '' && $params->get('link_titles')) : ?>
+                    <a href="<?php echo $item->link; ?>">
+                    <?php endif; ?>
+                    <?php echo $titleMaxLen == 0 ? $item->title : JHtml::_('string.truncate', $item->title, $titleMaxLen); ?>
+                    <?php if ($item->link !== '' && $params->get('link_titles')) : ?>
+                    </a>
+                    <?php endif; ?>
+                    </<?php echo $item_heading; ?>>
+                </div>
+                <?php
+                break;
+
+            case 'readmore':
+	            if($params->get('readmore')) :
+                ?>
+                <div class="item-articlenews-readmore">
+                    <a href="<?php echo $item->link; ?>"><?php echo $item->linkText; ?></a>
+                </div>
+	            <?php
+	            endif;
+                break;
+
+            case 'text':
+                ?>
+	            <div class="item-articlenews-text">
+		            <?php if (!$params->get('intro_only')) : ?>
+			            <?php echo $item->afterDisplayTitle; ?>
+		            <?php endif; ?>
+
+		            <?php echo $item->beforeDisplayContent; ?>
+
+		            <?php if ($params->get('show_introtext', '1')) : ?>
+			            <?php echo $contentMaxLen == 0 ?  $item->introtext : JHtml::_('string.truncate', $item->introtext, $contentMaxLen, true, false); ?>
+		            <?php endif; ?>
+
+		            <?php echo $item->afterDisplayContent; ?>
+
+                </div>
+                <?php
+                break;
+
+            default:
+                echo '<div>Informations '.$infos.' introuvable';
+                break;
+        }
+        ?>
+    <?php endforeach; ?>
+</div>
+
+</div>
---
+++ html/mod_articles_news/c3rb.php
@@ -0,0 +1,40 @@
+﻿<?php
+defined('_JEXEC') or die;
+
+// Nombre colonnes
+$nbCols = 2;
+
+// Taille max en nb caractères pour le titre si = 0 pas de limitation
+$titleMaxLen = 100;
+
+// Taille max en nb caractères pour le texte de l'article si = 0 pas de limitation
+$contentMaxLen = 200;
+
+// Ordre des infos à afficher
+$orderInfos = array(
+	'image',
+	'categorie',
+	'title',
+	'text',
+	'readmore'
+);
+
+// Définition des classes en fonction du nombre de colonnes
+$colDef = array(
+    2 => "col-xs-12 col-sm-12 col-md-6 col-lg-6",
+    3 => "col-xs-12 col-sm-12 col-md-4 col-lg-4",
+    4 => "col-xs-12 col-sm-12 col-md-3 col-lg-3",
+    6 => "col-xs-12 col-sm-12 col-md-2 col-lg-2"
+);
+
+?>
+
+<div class="newsflash<?php echo $moduleclass_sfx; ?>">
+    <div class="row">
+<?php
+        foreach ($list as $item) :
+               require JModuleHelper::getLayoutPath('mod_articles_news', '_c3rb_item');
+        endforeach;
+?>
+    </div>
+</div>
